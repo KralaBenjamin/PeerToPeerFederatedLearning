a2af2421bdf5cca4f7bc210c7c48738bfc637842
brandjak <brandjak@hu-berlin.de>
2023-06-17 12:09:08 +0200

    Added maintaining of own classes and neighbouring classes.

--- Peer.py
+++ Peer.py
@@ -11,8 +11,10 @@ class PeerNode:
         self.host = host
         self.port = port
         self.max_peers = max_peers
-        self.peers = set()
+        self.peers = {}
         self.connections = {}
+        self.classes = [0, 10]
+        self.weights = None
 
     async def start(self):
         server = await asyncio.start_server(self.handle_connection, self.host, self.port)
@@ -21,23 +23,24 @@ class PeerNode:
         async with server:
             await server.serve_forever()
 
-    async def receive_package(self, reader, peer_host, peer_port):
+    async def receive_package(self, reader):
         package_length_data = await reader.readexactly(4)
         package_length = int.from_bytes(package_length_data, "big")
 
         package_data = await reader.readexactly(package_length)
         package = pickle.loads(package_data)
 
+        address = package["ADDRESS"]
         action = package["ACTION"]
         load = package["LOAD"]
 
-        logging.info(f"Received message from {peer_host}:{peer_port} - Action: {action}, Load: {load}")
+        logging.info(f"Received message from {address} - Action: {action}, Load: {load}")
 
         return package
 
     async def send_package(self, writer, action="", load=None):
         package = {
-            "ADDRESS": (self.host, self.port),
+            "ADDRESS": (self.host, self.port, self.classes),
             "ACTION": action,
             "LOAD": load
         }
@@ -48,33 +51,35 @@ class PeerNode:
         writer.write(package_data)
         await writer.drain()
 
+    async def handle_request(self, package, writer):
+        if package["ACTION"] == "REQUEST WEIGHTS":
+            if self.weights is not None:
+                await self.send_package(writer, "WEIGHTS SEND", self.weights)
+
+            else:
+                await self.send_package(writer, "UNABLE TO SEND WEIGHTS")
+        elif package["ACTION"] == "SEEK PEERS":
+            await self.send_package(writer, "PEERS SEND", self.peers)
+
     async def handle_connection(self, reader, writer):
         peer_host, peer_port = writer.get_extra_info('peername')
         logging.info(f"Incoming connection from {peer_host}:{peer_port}")
 
-        initial_package = await self.receive_package(reader, peer_host, peer_port)
+        initial_package = await self.receive_package(reader)
 
         if len(self.peers) < self.max_peers:
             logging.info(f"Accepting connection from {peer_host}:{peer_port}")
 
-            response_load = None
-            if initial_package["ACTION"] == "SEEK PEERS":
-                response_load = ",".join([f"{host}:{port}" for host, port in self.peers])
+            await self.send_package(writer, "CONNECTION ACCEPTED",
+                                    self.peers if initial_package["ACTION"] == "SEEK PEERS" else None)
 
-            await self.send_package(writer, "CONNECTION ACCEPTED", response_load)
-
-            self.peers.add(initial_package["ADDRESS"])
+            self.peers[initial_package["ADDRESS"][0] + ":" + initial_package["ADDRESS"][1]] = \
+                initial_package["ADDRESS"][2]
 
             try:
                 while True:
-                    package = await self.receive_package(reader, peer_host, peer_port)
-
-                    action = package["ACTION"]
-                    load = package["LOAD"]
-
-                    logging.info(f"Received message from {peer_host}:{peer_port} - Action: {action}, Load: {load}")
-
-                    # Handle the received message here
+                    package = await self.receive_package(reader)
+                    await self.handle_request(package, writer)
 
             except asyncio.IncompleteReadError as e:
                 logging.error(f"Incomplete Read error: {e}")
@@ -82,9 +87,7 @@ class PeerNode:
 
         else:
             logging.error(f"Max peers limit reached for {peer_host}:{peer_port}")
-            response_load = ",".join([f"{host}:{port}" for host, port in self.peers])
-
-            await self.send_package(writer, "CONNECTION REFUSED", response_load)
+            await self.send_package(writer, "CONNECTION REFUSED", self.peers)
 
         logging.info(f"Connection with {peer_host}:{peer_port} closed")
         writer.close()
@@ -93,15 +96,11 @@ class PeerNode:
     async def connect_to_peer(self, peer_host, peer_port, init=False):
         try:
             reader, writer = await asyncio.open_connection(peer_host, peer_port)
-
             logging.info(f"Connected to peer {peer_host}:{peer_port}")
 
             await self.send_package(writer, "SEEK PEERS" if init else "SEEK CONNECTION")
 
-            connection_id = f"{peer_host}:{peer_port}"
-            self.connections[connection_id] = (reader, writer)
-
-            response_package = await self.receive_package(reader, peer_host, peer_port)
+            response_package = await self.receive_package(reader)
 
             if response_package["ACTION"] == "CONNECTION ACCEPTED":
                 connection_id = f"{peer_host}:{peer_port}"
@@ -109,17 +108,17 @@ class PeerNode:
 
             received_load = response_package["LOAD"]
             if received_load:
-                received_peers = received_load.split(",")
-                for received_peer in received_peers:
-                    if received_peer:
-                        host, peer = received_peer.split(":")
-                        asyncio.create_task(self.connect_to_peer(host, peer))
+                for received_peer in received_load.keys():
+                    host, peer = received_peer.split(":")
+                    classes = received_load[received_peer]
+                    # TODO use classes to check which to connect to
+                    asyncio.create_task(self.connect_to_peer(host, peer))
 
             if response_package["ACTION"] == "CONNECTION ACCEPTED":
                 connection_id = f"{peer_host}:{peer_port}"
                 self.connections[connection_id] = (reader, writer)
                 while True:
-                    package = await self.receive_package(reader, peer_host, peer_port)
+                    package = await self.receive_package(reader)
                 # Start sending/receiving messages with the connected peer
 
         except (ConnectionRefusedError, asyncio.TimeoutError):
@@ -149,7 +148,6 @@ class PeerNode:
 
 
 async def main(port, connecting_port):
-
     node = PeerNode('localhost', port, max_peers=2)
 
     server_task = asyncio.create_task(node.start())
@@ -164,6 +162,7 @@ async def main(port, connecting_port):
     # Start sending/receiving messages with connected peers
 
     await server_task
+
 
 if __name__ == '__main__':
     port = 8000
